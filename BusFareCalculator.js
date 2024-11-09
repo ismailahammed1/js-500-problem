cfa52931281147b181df88e3fbab02129fd92ee2
ismailahammed1 <ismailahmmedroman@gmail.com>
2024-11-09 09:42:38 +0600

    condittional tsk

---
+++ BusFareCalculator.js
@@ -0,0 +1,11 @@
+/***
+
+Ticket fare Calculator
+    - Children (age < 10): free
+    - Students get a 50% discount
+    - Senior citizens (age >= 60) gets a 15% Discount
+    - Otherwise Regular ticket fare 800 tk
+*/
+
+
+console.log(6 != 6);
