179744c06cca42f8283a27b2d2519d9e50b8304a
ismailahammed1 <ismailahmmedroman@gmail.com>
2024-11-10 12:27:10 +0600

    TerneeryNumber

--- BusFareCalculator.js
+++ BusFareCalculator.js
@@ -6,6 +6,15 @@ Ticket fare Calculator
     - Senior citizens (age >= 60) gets a 15% Discount
     - Otherwise Regular ticket fare 800 tk
 */
-
-
-console.log(6 != 6);
+const age=90
+if (age<10) {
+    console.log("free");
+    
+} else if (age>=60) {
+    console.log("free");
+    
+} 
+else {
+    console.log("ticket fare 800 tK");
+    
+}
