4a3d4b6e44f5a5e7e173636790a92833517ba54d
ismailahammed1 <ismailahmmedroman@gmail.com>
2024-11-13 15:39:26 +0600

    60 time show Use forloop

--- loopTry/evenNumber.js
+++ loopTry/evenNumber.js
@@ -14,12 +14,19 @@
 //   console.log("hello", i);
 // }
 
-for (let i = 55; i <= 85; i++) {
-  if ( i % 5 === 0) {
-    continue;
-  }
-  console.log(i);
+// for (let i = 55; i <= 85; i++) {
+//   if ( i % 5 === 0) {
+//     continue;
+//   }
+//   console.log(i);
   
-}
+// }
 // Skip even numbers and numbers divisible by 5 } console.log(i);
 // Print odd numbers not divisible by
+
+
+// "I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+// for (let index = 1; index <= 60; index++) {
+//  console.log("I will invest at least 6 hrs every single day for next 60 days!",index);
+   
+// }
