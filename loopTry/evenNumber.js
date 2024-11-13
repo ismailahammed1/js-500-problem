96e7ff500bd42f46502cedef44b6ca201b8d5e87
ismailahammed1 <ismailahmmedroman@gmail.com>
2024-11-13 15:35:23 +0600

    even odd skip use contine

---
+++ loopTry/evenNumber.js
@@ -0,0 +1,25 @@
+// let num = 40;
+// for (let i = 0; i <= num; i++) {
+//   if (i % 2 !== 0) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+// let num2 = 40;
+// for (let i = 0; i <= num2; i++) {
+//   if (i % 2 === 0) {
+//     continue;
+//   }
+//   console.log("hello", i);
+// }
+
+for (let i = 55; i <= 85; i++) {
+  if ( i % 5 === 0) {
+    continue;
+  }
+  console.log(i);
+  
+}
+// Skip even numbers and numbers divisible by 5 } console.log(i);
+// Print odd numbers not divisible by
