6a22c422a1810e38ae4171654e18f93951df60f0
ismailahammed1 <ismailahmmedroman@gmail.com>
2024-11-13 15:41:53 +0600

    forloop done

--- loopTry/evenNumber.js
+++ loopTry/evenNumber.js
@@ -1,32 +1,47 @@
-// let num = 40;
-// for (let i = 0; i <= num; i++) {
-//   if (i % 2 !== 0) {
-//     continue;
-//   }
-//   console.log(i);
-// }
+let num = 40;
+for (let i = 0; i <= num; i++) {
+  if (i % 2 !== 0) {
+    continue;
+  }
+  console.log(i);
+}
 
-// let num2 = 40;
-// for (let i = 0; i <= num2; i++) {
-//   if (i % 2 === 0) {
-//     continue;
-//   }
-//   console.log("hello", i);
-// }
+let num2 = 40;
+for (let i = 0; i <= num2; i++) {
+  if (i % 2 === 0) {
+    continue;
+  }
+  console.log("hello", i);
+}
 
-// for (let i = 55; i <= 85; i++) {
-//   if ( i % 5 === 0) {
-//     continue;
-//   }
-//   console.log(i);
+for (let i = 55; i <= 85; i++) {
+  if ( i % 5 === 0) {
+    continue;
+  }
+  console.log(i);
   
-// }
+}
 // Skip even numbers and numbers divisible by 5 } console.log(i);
 // Print odd numbers not divisible by
 
 
 // "I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
-// for (let index = 1; index <= 60; index++) {
-//  console.log("I will invest at least 6 hrs every single day for next 60 days!",index);
+for (let index = 1; index <= 60; index++) {
+ console.log("I will invest at least 6 hrs every single day for next 60 days!",index);
    
-// }
+}
+// oddd
+for (let index = 61; index < 100; index++) {
+  if (index%2===0) {
+    console.log(index);
+    
+  }
+  
+}
+for (let index = 61; index < 100; index++) {
+  if (index%2!==0) {
+    console.log(index);
+    
+  }
+  
+}
