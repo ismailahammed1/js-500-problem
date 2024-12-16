d0d5934028acced3d5cc64f942c43aca41364f25
ismailahammed1 <ismailahmmedroman@gmail.com>
2024-12-16 15:55:57 +0600

    findFilterNUmber

---
+++ taskPH/findWithFilter.js
@@ -0,0 +1,8 @@
+const arrayFilter=[12,16,10,20,25,30,40,45,50,66,60,77,70,80,88,90,99]
+
+const  findFilterNUmber=arrayFilter.filter(num=>{
+    if (num%10===0) {
+        console.log(num); 
+    }
+}
+)
