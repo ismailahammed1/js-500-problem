42ed4a7d0839f26ddab40482d03bdcff98611f89
ismailahammed1 <ismailahmmedroman@gmail.com>
2024-12-17 16:08:10 +0600

    people.reduce

--- taskPH/findEvenAndmakeOdd.js
+++ taskPH/findEvenAndmakeOdd.js
@@ -9,7 +9,35 @@ for (let i = 0; i < findEvenandMakeOdd.length; i++) {
 const findeven=findEvenandMakeOdd.map(num=>{
     if (num%2==0) {
         const element = num+1;
-        console.log(element);
+       // console.log(element);
     } 
 })
+
+// task 3 senior find with filter
+const instructor = [
+{name:"nodi", age:28, position:'senior'},
+{name:"phodi", age:25, position:'junior'},
+{name:"kodi", age:29, position:'senior'},
+]
+const FindInstructor=instructor.map(person=>{
+    if (person.position==='senior') {
+       // console.log(person.name + " is a "+ person.position + " instructor");
+    }
+})
+
+const people=[
+    {name:'Meena', age:20},
+    {name:'feena', age:10},
+    {name:'jeena', age:22},
+]
+let sum=0
+for (const item of people) {
+    sum +=item.age
+  
+}
+//console.log(sum);
+const findAvarage=people.reduce((prev, curr)=> prev+curr.age ,0)
+console.log(findAvarage);
+
+
 
--- taskPH/findWithFilter.js
+++ taskPH/findWithFilter.js
@@ -2,7 +2,25 @@ const arrayFilter=[12,16,10,20,25,30,40,45,50,66,60,77,70,80,88,90,99]
 
 const  findFilterNUmber=arrayFilter.filter(num=>{
     if (num%10===0) {
-        console.log(num); 
+    //    console.log("filter:",num); 
     }
 }
 )
+const  findFindNUmber=arrayFilter.find(num=>{
+    if (num%10===0) {
+    //    console.log("find:",num); 
+    }
+}
+)
+
+
+
+
+
+for (const item of arrayFilter) {
+    if (item % 10 === 0) {
+
+        console.log(item);
+    }
+}
+
